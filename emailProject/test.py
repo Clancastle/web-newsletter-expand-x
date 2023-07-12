17f657d7dd9dc7152d919fe004190eb71d03ca03
Clancastle <dennisungureanu0@gmail.com>
2023-07-12 12:45:31 -0500

    comments

--- emailProject/test.py
+++ emailProject/test.py
@@ -2,6 +2,9 @@ import os
 from flask import Flask, Blueprint, jsonify
 # from project import app
 
+#GET from index.html, GET the 4 values , age, fname, lname, email
+
+
 path_cwd = os.path.dirname(os.path.realpath(__file__))
 path_templates = os.path.join(path_cwd,"templates")
 path_static = os.path.join(path_cwd,"static")
