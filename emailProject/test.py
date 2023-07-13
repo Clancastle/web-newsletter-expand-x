0a446ba760ade62de938a9aa2f69365b5b80e506
Clancastle <dennisungureanu0@gmail.com>
2023-07-12 20:40:24 -0500

    k

--- emailProject/test.py
+++ emailProject/test.py
@@ -1,20 +1,23 @@
 import os
-from flask import Flask, Blueprint, jsonify
-# from project import app
-
-#GET from index.html, GET the 4 values , age, fname, lname, email
-
+from flask import Flask, Blueprint, jsonify, request
 
 path_cwd = os.path.dirname(os.path.realpath(__file__))
-path_templates = os.path.join(path_cwd,"templates")
-path_static = os.path.join(path_cwd,"static")
+path_templates = os.path.join(path_cwd, "templates")
+path_static = os.path.join(path_cwd, "static")
 
 Func = Blueprint('func', __name__, static_folder=path_static, template_folder=path_templates)
-@Func.route('/func', methods=['GET','POST'])
+
+@Func.route('/func', methods=['POST'])
 def func():
+    dataGet = request.get_json(force=True)
 
-    dataGet = '' if not request.get_json(force=True) else request.get_json(force=True)
+    # Process the received data
+    email = dataGet.get('email')
+    fname = dataGet.get('fname')
+    lname = dataGet.get('lname')
+    age = dataGet.get('age')
 
-    dataReply = {'backend_data':'some_data'}
+    # Perform any necessary actions with the data
 
+    dataReply = {'backend_data': 'some_data'}
     return jsonify(dataReply)
