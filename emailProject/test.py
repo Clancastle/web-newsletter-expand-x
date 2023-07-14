1324aa2c4ffd362129678fe20f53d9fba5a927cc
Clancastle <dennisungureanu0@gmail.com>
2023-07-14 12:25:03 -0500

    new idea

--- emailProject/test.py
+++ emailProject/test.py
@@ -1,23 +1,7 @@
-import os
-from flask import Flask, Blueprint, jsonify, request
+import re
 
-path_cwd = os.path.dirname(os.path.realpath(__file__))
-path_templates = os.path.join(path_cwd, "templates")
-path_static = os.path.join(path_cwd, "static")
+# Validate strong password
+password_pattern = "^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$"
+print(re.match(password_pattern, 'secret')) # Returns None
+print(re.match(password_pattern, '-Secr3t.')) # Returns Match object
 
-Func = Blueprint('func', __name__, static_folder=path_static, template_folder=path_templates)
-
-@Func.route('/func', methods=['POST'])
-def func():
-    dataGet = request.get_json(force=True)
-
-    # Process the received data
-    email = dataGet.get('email')
-    fname = dataGet.get('fname')
-    lname = dataGet.get('lname')
-    age = dataGet.get('age')
-
-    # Perform any necessary actions with the data
-
-    dataReply = {'backend_data': 'some_data'}
-    return jsonify(dataReply)
