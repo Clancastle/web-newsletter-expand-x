046f74f1ff5eddadf1f024f5690bc221f53c3dda
Clancastle <dennisungureanu0@gmail.com>
2023-07-15 15:50:30 -0500

    jsonify

--- emailProject/auth.py
+++ emailProject/auth.py
@@ -1,18 +1,21 @@
-from flask import request, render_template, Blueprint, redirect
+from flask import request, render_template, Blueprint, redirect, jsonify
 
 auth = Blueprint('auth', __name__)
 
 @auth.route('/k-signup', methods=['GET', 'POST'])
 def signup():
-
-    # data = request.get_json()
-    # email = data.get('email')
-    # age = data.get('age')
-    # fname = data.get('fname')
-    # lname = data.get('lname')
-
     if request.method == 'POST':
+        email = request.form.get('email')
+        age = request.form.get('age')
+        fname = request.form.get('fname')
+        lname = request.form.get('lname')
+
         print(request.form)
+
+        if not email or not age or not fname or not lname:
+            return jsonify({'success': False, 'message': 'All fields are required.'})
+
+        return jsonify({'success': True, 'message': 'Form submitted successfully.'})
 
     if request.method == "GET":
         pass
--- emailProject/views.py
+++ emailProject/views.py
@@ -5,4 +5,4 @@ views = Blueprint('views', __name__)
 
 @views.route('/', methods=['GET'])
 def home():
-    return render_template('base.html')
+    return render_template('mainpage.html')
