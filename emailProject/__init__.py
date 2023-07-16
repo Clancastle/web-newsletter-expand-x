40871617319e3448cfef336de079dff41ad67884
Clancastle <dennisungureanu0@gmail.com>
2023-07-16 14:19:35 -0500

    db

--- emailProject/__init__.py
+++ emailProject/__init__.py
@@ -1,10 +1,13 @@
 from flask import Flask, render_template
 import sqlite3
 from flask_login import LoginManager
+from flask_sqlalchemy import SQLAlchemy
 
 def create_app():
     app = Flask(__name__)
     app.config['SECRET_KEY'] = 'den'
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///emails.db'  # Specify the database URI
+
 
     from .views import views
     from .auth import auth
@@ -14,7 +17,9 @@ def create_app():
 
     return app
 
-app = Flask(__name__)
+
+app = create_app()
+
 @app.route('/k-signup')
 def takeData():
     connect = sqlite3.connect('emails.db')
@@ -28,20 +33,8 @@ def takeData():
     return render_template('signup.html')
 
 def create_table():
-    connect = sqlite3.connect('emails.db')
-    cursor = connect.cursor()
-
-    # Create the email_list table if it doesn't exist
-    cursor.execute('''
-        CREATE TABLE IF NOT EXISTS email_list (
-            id INTEGER PRIMARY KEY AUTOINCREMENT,
-            email TEXT NOT NULL
-        )
-    ''')
-
-    connect.commit()
-    cursor.close()
-    connect.close()
+    db = SQLAlchemy(app)  # Initialize SQLAlchemy with the app
+    db.create_all()  # Create all tables based on the defined models
 
 if __name__ == '__main__':
     create_table()
