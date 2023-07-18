0a4f6422f287588932da3f15203f13088f1d5a24
Clancastle <dennisungureanu0@gmail.com>
2023-07-17 21:08:02 -0500

    fk in hell
    redirect works with js. ig js is good for some things

--- emailProject/static/signup.js
+++ emailProject/static/signup.js
@@ -5,76 +5,90 @@ const lname = document.getElementById('lname');
 const age = document.getElementById('age');
 const submitButton = document.getElementById('button_submit');
 
-submitButton.disabled
+submitButton.disabled = true; // Disable submit button initially
 
 form.addEventListener('submit', e => {
-    e.preventDefault();
-    validateInputs();
+  e.preventDefault();
+  validateInputs();
 });
 
 const setError = (element, message) => {
-    const cntrl = element.parentElement;
-    const errorDisplay = cntrl.querySelector('.error');
+  const cntrl = element.parentElement;
+  const errorDisplay = cntrl.querySelector('.error');
 
-    errorDisplay.innerText = message;
-    cntrl.classList.add('error');
-    cntrl.classList.remove('success');
+  errorDisplay.innerText = message;
+  cntrl.classList.add('error');
+  cntrl.classList.remove('success');
+
+  submitButton.disabled = true; // Disable submit button when there's an error
 };
 
 const setSuccess = element => {
-    const cntrl = element.parentElement;
-    const errorDisplay = cntrl.querySelector('.error');
+  const cntrl = element.parentElement;
+  const errorDisplay = cntrl.querySelector('.error');
 
-    errorDisplay.innerText = '';
-    cntrl.classList.add('success');
-    cntrl.classList.remove('error');
+  errorDisplay.innerText = '';
+  cntrl.classList.add('success');
+  cntrl.classList.remove('error');
+
+  checkFormValidity(); // Check form validity when inputs are successful
 };
 
 const isValidEmail = email => {
-    const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
-    return re.test(String(email).toLowerCase());
+  const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+  return re.test(String(email).toLowerCase());
 };
 
 const validateInputs = () => {
-    const emailValue = email.value.trim();
-    const ageValue = parseInt(age.value.trim(), 10);
-    const fnameValue = fname.value.trim();
-    const lnameValue = lname.value.trim();
+  const emailValue = email.value.trim();
+  const ageValue = parseInt(age.value.trim(), 10);
+  const fnameValue = fname.value.trim();
+  const lnameValue = lname.value.trim();
 
-    if (fnameValue === "") {
-        setError(fname, "First Name is required");
-    } else if (fnameValue.length < 3 || fnameValue.length > 20 || /\d/.test(fnameValue) || /[\d\W]/.test(fnameValue)) {
-        setError(fname, "First Name must be between 3 and 20 characters and should not contain numbers");
-    } else {
-        setSuccess(fname);
-    }
+  if (fnameValue === "") {
+    setError(fname, "First Name is required");
+  } else if (fnameValue.length < 3 || fnameValue.length > 20 || /\d/.test(fnameValue) || /[\d\W]/.test(fnameValue)) {
+    setError(fname, "First Name must be between 3 and 20 characters and should not contain numbers");
+  } else {
+    setSuccess(fname);
+  }
 
-    if (lnameValue === "") {
-        setError(lname, "Last Name is required");
-    } else if (lnameValue.length < 3 || lnameValue.length > 25 || /\d/.test(lnameValue) || /[\d\W]/.test(lnameValue)) {
-        setError(lname, "Last Name must be between 3 and 25 characters and should not contain numbers");
-    } else {
-        setSuccess(lname);
-    }
+  if (lnameValue === "") {
+    setError(lname, "Last Name is required");
+  } else if (lnameValue.length < 3 || lnameValue.length > 25 || /\d/.test(lnameValue) || /[\d\W]/.test(lnameValue)) {
+    setError(lname, "Last Name must be between 3 and 25 characters and should not contain numbers");
+  } else {
+    setSuccess(lname);
+  }
 
-    if (ageValue <= 0 || isNaN(ageValue) || ageValue >= 100) {
-        setError(age, "Invalid Age");
-    } else {
-        setSuccess(age);
-    }
+  if (ageValue <= 0 || isNaN(ageValue) || ageValue >= 100) {
+    setError(age, "Invalid Age");
+  } else {
+    setSuccess(age);
+  }
 
-    if (emailValue === "") {
-        setError(email, "Email is required");
-    } else if (!isValidEmail(emailValue)) {
-        setError(email, "Make sure that is your correct email.");
-    } else {
-        setSuccess(email);
-    }
-    const inputs = document.querySelectorAll('.cntrl input');
-    const isValid = Array.from(inputs).every(input => input.parentElement.classList.contains('success'));
-
-    submitButton.disabled = !isValid;
+  if (emailValue === "") {
+    setError(email, "Email is required");
+  } else if (!isValidEmail(emailValue)) {
+    setError(email, "Make sure that is your correct email.");
+  } else {
+    setSuccess(email);
+  }
 };
+
+const checkFormValidity = () => {
+  const inputs = document.querySelectorAll('.cntrl input');
+  const isValid = Array.from(inputs).every(input => input.parentElement.classList.contains('success'));
+
+  submitButton.disabled = !isValid; // Enable/disable submit button based on form validity
+
+  if (isValid) {
+    form.submit(); // Submit the form if it's valid
+    // Alternatively, you can use:
+    // window.location.href = '/'; // Redirect to the home page
+  }
+};
+
 // Add event listeners to input fields
 email.addEventListener('input', validateInputs);
 fname.addEventListener('input', validateInputs);
