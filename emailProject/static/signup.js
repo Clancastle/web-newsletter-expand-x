03eea56120507049bdac7a758ee570ba537c68e9
Clancastle <dennisungureanu0@gmail.com>
2023-07-18 18:40:07 -0500

    new format, plus just validate once.

--- emailProject/static/signup.js
+++ emailProject/static/signup.js
@@ -55,31 +55,31 @@ const validateInputs = () => {
   const lnameValue = lname.value.trim();
 
   if (fnameValue === "") {
-    setError(fname, "First Name is required");
+    setError(email, "Invalid data");
   } else if (fnameValue.length < 3 || fnameValue.length > 20 || /\d/.test(fnameValue) || /[\d\W]/.test(fnameValue)) {
-    setError(fname, "First Name must be between 3 and 20 characters and should not contain numbers");
+    setError(email, "Invalid data");
   } else {
     setSuccess(fname);
   }
 
   if (lnameValue === "") {
-    setError(lname, "Last Name is required");
+    setError(email, "Invalid data");
   } else if (lnameValue.length < 3 || lnameValue.length > 25 || /\d/.test(lnameValue) || /[\d\W]/.test(lnameValue)) {
-    setError(lname, "Last Name must be between 3 and 25 characters and should not contain numbers");
+    setError(email, "Invalid data");
   } else {
     setSuccess(lname);
   }
 
   if (ageValue <= 0 || isNaN(ageValue) || ageValue >= 100) {
-    setError(age, "Invalid Age");
+    setError(email, "Invalid data");
   } else {
     setSuccess(age);
   }
 
   if (emailValue === "") {
-    setError(email, "Email is required");
+    setError(email, "Invalid data");
   } else if (!isValidEmail(emailValue)) {
-    setError(email, "Make sure that is your correct email.");
+    setError(email, "Invalid data");
   } else {
     setSuccess(email);
   }
@@ -108,3 +108,42 @@ email.addEventListener('input', validateInputs);
 fname.addEventListener('input', validateInputs);
 lname.addEventListener('input', validateInputs);
 age.addEventListener('input', validateInputs);
+
+
+
+//const validateInputs = () => {
+//  const emailValue = email.value.trim();
+//  const ageValue = parseInt(age.value.trim(), 10);
+//  const fnameValue = fname.value.trim();
+//  const lnameValue = lname.value.trim();
+//
+//  if (fnameValue === "") {
+//    setError(fname, "First Name is required");
+//  } else if (fnameValue.length < 3 || fnameValue.length > 20 || /\d/.test(fnameValue) || /[\d\W]/.test(fnameValue)) {
+//    setError(fname, "First Name must be between 3 and 20 characters and should not contain numbers");
+//  } else {
+//    setSuccess(fname);
+//  }
+//
+//  if (lnameValue === "") {
+//    setError(lname, "Last Name is required");
+//  } else if (lnameValue.length < 3 || lnameValue.length > 25 || /\d/.test(lnameValue) || /[\d\W]/.test(lnameValue)) {
+//    setError(lname, "Last Name must be between 3 and 25 characters and should not contain numbers");
+//  } else {
+//    setSuccess(lname);
+//  }
+//
+//  if (ageValue <= 0 || isNaN(ageValue) || ageValue >= 100) {
+//    setError(age, "Invalid Age");
+//  } else {
+//    setSuccess(age);
+//  }
+//
+//  if (emailValue === "") {
+//    setError(email, "Email is required");
+//  } else if (!isValidEmail(emailValue)) {
+//    setError(email, "Make sure that is your correct email.");
+//  } else {
+//    setSuccess(email);
+//  }
+//};
